3f22a5ddd58bc91916149112e4f4c5034ec23c26
Zandzakje <DamianTomasila@hotmail.com>
2023-10-13 14:36:06 +0200

    initial push
    Added base project files

---
+++ src/main/java/umu/kwetter/heart/HeartApplication.java
@@ -0,0 +1,13 @@
+package umu.kwetter.heart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartApplication.class, args);
+	}
+
+}
